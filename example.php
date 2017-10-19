2cd9a8565aa8cf56e2f0835530654b9eda6a258b
Enea Dolcini <eneadolcini@gmail.com>
2017-10-19 20:36:23 +0200

    Added an example

---
+++ example.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('class.php');
+
+$API = new API(['Hey', 'this' => 'is an example']);
+
+$API->setPassword('Example');
+$API->set(['ok' => true]);
+$API->set(['result' => ['example' => 'first example', 'example2' => 'second example']]);
+$API->run();
+
+?>
