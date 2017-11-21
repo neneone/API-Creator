61bb71372c8f57c7d268a6829453417c683e8f96
Nicolò <nick05@tiscali.it>
2017-11-21 18:55:00 +0100

    Update example.php

--- example.php
+++ example.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once('class.php');
+require_once __DIR__ . '/vendor/autoload.php';
 
-$API = new API(['Hey', 'this' => 'is an example']);
+$API = new neneone\apicreator\API(['ok' => true]);
 
-$API->setPassword('Example');
-$API->set(['ok' => true]);
-$API->set(['result' => ['example' => 'first example', 'example2' => 'second example']]);
+//$API->setPassword('Example'); 🤔
+$API->set(['working' => true]);
+$API->set(['result' => ['one' => true, 'second' => false]]);
 $API->run();
 
-?>
+//{"ok":true,"working":true,"result":{"one":true,"example2":"second example"}}
