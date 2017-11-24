e2e017f3ff4e709a7387d29bd59686663adb2f99
TRIGGEREDNICK <nick05@tiscali.it>
2017-11-24 16:51:45 +0000

    Apply fixes from StyleCI

--- example.php
+++ example.php
@@ -1,18 +1,17 @@
 <?php
+
 error_reporting(0);
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 try {
-    
     $API = new neneone\apicreator\API(['ok' => true]);
-    
+
     //$API->setPassword('Example'); 🤔
     $API->set(['working' => true]);
     $API->set(['result' => ['one' => true, 'second' => false]]);
     $API->run();
-    
-    //{"ok":true,"working":true,"result":{"one":true,"example2":"second example"}}
 
-} catch(Exception $e){
+    //{"ok":true,"working":true,"result":{"one":true,"example2":"second example"}}
+} catch (Exception $e) {
     echo $e->getMessage();
 }
--- src/class.php
+++ src/class.php
@@ -1,53 +1,59 @@
 <?php
 
 namespace neneone\apicreator;
-use \Exception;
 
-class API {
-  
-  public $base;
+use Exception;
 
-  public function __construct($base) {
-    $this->base = $base;
-  }
+class API
+{
+    public $base;
 
-  public function setPassword($password) {
-    $methods = [$_GET, $_POST];
-
-    foreach($methods as $method) {
-      if(in_array($password, $method)) {
-        $success = true;
-      }
+    public function __construct($base)
+    {
+        $this->base = $base;
     }
 
-    if(!$success) {
-      throw new Exception('Invalid password');
+    public function setPassword($password)
+    {
+        $methods = [$_GET, $_POST];
+
+        foreach ($methods as $method) {
+            if (in_array($password, $method)) {
+                $success = true;
+            }
+        }
+
+        if (!$success) {
+            throw new Exception('Invalid password');
+        }
     }
-  }
 
-  public function set($parameters) {
-    $base = $this->base;
+    public function set($parameters)
+    {
+        $base = $this->base;
 
-    if(!$parameters){
-      throw new Exception('Invalid parameters');
-      return false;
+        if (!$parameters) {
+            throw new Exception('Invalid parameters');
+            return false;
+        }
+
+        foreach ($parameters as $key => $value) {
+            if (is_numeric($key)) {
+                $base[] = $value;
+            } else {
+                $base[$key] = $value;
+            }
+        }
+        $this->base = $base;
+
+        return true;
     }
 
-    foreach($parameters as $key => $value) {
-      if(is_numeric($key)) {
-        $base[] = $value;
-      } else {
-        $base[$key] = $value;
-      }
-    }
-    $this->base = $base;
-    return true;
-  }
-
-  public function run() {
-    header('content-type: application/json');
-    echo json_encode($this->base);
-    $this->base = false; //idk
+    public function run()
+    {
+        header('content-type: application/json');
+        echo json_encode($this->base);
+        $this->base = false; //idk
     return true; // BOH
-  }
+    }
 }
