c12f6a6ccc461ac9ffe3f174ba20b9634515e9d6
Enea Dolcini <eneadolcini@gmail.com>
2018-01-18 14:37:51 +0100

    Update setPassword() in class.php
    The new use is setPassword('Password', 'Field that contains the password');
    The Field isn't required. If you don't define it, it is 'password'.

--- class.php
+++ class.php
@@ -1,9 +1,6 @@
 <?php
-
 class API {
-
   private $base;
-
   public function __construct($base) {
     if(is_array($base)) {
       $this->base = $base;
@@ -11,36 +8,27 @@ class API {
       $this->fail('Invalid base');
     }
   }
-
-  public function setPassword($password) {
+  public function setPassword($password, $field = 'password') {
     $methods = [$_GET, $_POST];
-
     foreach($methods as $method) {
-      if(in_array($password, $method)) {
+      if($method[$field] == $password) {
         $success = true;
       }
     }
-
     if(!$success) {
       $this->fail('Invalid password');
     }
   }
-
   public function fail($error) {
     $this->error = $error;
     exit($this->error);
   }
-
   public function set($parameters) {
     $base = $this->base;
-
     if(is_array($parameters) == false) {
-
       return false;
-
       $this->fail('Invalid parameters');
     }
-
     foreach($parameters as $key => $value) {
       if(is_numeric($key)) {
         $base[] = $value;
@@ -48,16 +36,12 @@ class API {
         $base[$key] = $value;
       }
     }
-
     $this->base = $base;
-
     return true;
   }
-
   public function run() {
     echo json_encode($this->base);
     $this->base = false;
   }
 }
-
  ?>
