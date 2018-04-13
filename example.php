04af07aff86cbc0f9747a8daf2101fbcf25fdd2b
Nicolò <nick05@tiscali.it>
2018-04-13 18:01:33 +0200

    Update example.php

--- example.php
+++ example.php
@@ -6,12 +6,10 @@ require_once __DIR__.'/vendor/autoload.php';
 try {
     $API = new neneone\apicreator\API(['ok' => true]);
 
-    //$API->setPassword('Example'); 🤔
     $API->set(['working' => true]);
     $API->set(['result' => ['one' => true, 'second' => false]]);
-    $API->run();
-
-    //{"ok":true,"working":true,"result":{"one":true,"example2":"second example"}}
+    print_r($API->run()); /* {"ok":true,"working":true,"result":{"one":true,"example2":"second example"}} */
+    
 } catch (Exception $e) {
     echo $e->getMessage();
 }
