dc10cc0648366c159d1fd30772a76aaec02552e6
TRIGGEREDNICK <nick05@tiscali.it>
2018-04-13 16:01:38 +0000

    Apply fixes from StyleCI

--- example.php
+++ example.php
@@ -9,7 +9,6 @@ try {
     $API->set(['working' => true]);
     $API->set(['result' => ['one' => true, 'second' => false]]);
     print_r($API->run()); /* {"ok":true,"working":true,"result":{"one":true,"example2":"second example"}} */
-    
 } catch (Exception $e) {
     echo $e->getMessage();
 }
